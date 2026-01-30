63ebbd354c6269eb7d1ae13dba8f6f6398889075
Manonmani Natarajan <manonmanin05@gmail.com>
2026-01-30 18:42:21 +0200

    Create problem1.py
    Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
    
    CASE 1:
    Input:  2 Name y
    Expected Output:
    2
    Name
    y
    
    CASE 2:
    Input: we 2 k
    Expected Output:
    Incorrect type.try again!

---
+++ Programming Basics/Problem Set 1 /problem1.py
@@ -0,0 +1,11 @@
+try:
+    print("Enter Interger")
+    number=int(input())
+    print("Enter string")
+    word=input()
+    print("Enter char")
+    character=input()
+except:
+    print("Incorrect type.try again!")
+    
+print(number,"\n",word,"\n",character)
